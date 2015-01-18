39060389b7e689ab1ca39cac860b1694c703f373
DrummerMC <drummermods@icloud.com>
2015-01-18 17:07:39 +0100

    Upload 2

--- src/main/java/drummermc/debug/Main.java
+++ src/main/java/drummermc/debug/Main.java
@@ -1,12 +1,12 @@
-package com.example.examplemod;
+package drummermc.debug;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Main.MODID, version = Main.VERSION)
+public class Main
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
