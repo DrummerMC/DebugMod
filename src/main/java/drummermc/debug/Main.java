bc372e7bd7252279fa05ce7403e596acd0d8e1dc
Fabian Siering <Fabian Siering@FSPCMAIN14>
2015-01-18 18:01:58 +0100

    x

--- src/main/java/drummermc/debug/Main.java
+++ src/main/java/drummermc/debug/Main.java
@@ -4,6 +4,7 @@ import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main
